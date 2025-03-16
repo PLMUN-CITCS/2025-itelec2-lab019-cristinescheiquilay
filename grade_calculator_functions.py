4fe3b45076c4c2f7e7c9c7897eefe09845f56ed6
cristinescheiquilay <scheiyaliuq@gmail.com>
2025-03-16 17:10:08 +0800

    Submitting Python Week 05 - Laboratory # 19

--- grade_calculator_functions.py
+++ grade_calculator_functions.py
@@ -4,14 +4,11 @@ def get_integer_input() -> int:
             return int(input("Enter an integer: "))
         except ValueError:
             print("Error: Please enter a valid integer.")
-
 def check_even_odd(number: int) -> str:
     return f"{number} is an Even number." if number % 2 == 0 else f"{number} is an Odd number."
-
 def main():
     number = get_integer_input()
     result = check_even_odd(number)
     print(result)
-
 if _name_ == "_main_":
     main()
